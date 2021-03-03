4389ff277fac8c110e9f586334d34766b91d6c08
Ajith madhan <ajithmadhan.dev113@gmail.com>
2021-03-03 23:13:35 +0530

    added skeletons of routes and controllers

---
+++ app.js
@@ -0,0 +1,35 @@
+require('dotenv').config()
+const mongoose = require('mongoose');
+const express = require("express")
+const app =express()
+const bodyParser=require("body-parser");
+const cookieParser=require("cookie-parser");
+const cors=require("cors");
+
+
+const authRoutes=require("./routes/auth");
+
+// db connection
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+    useCreateIndex:true
+}).then(()=>{
+    console.log("DB CONNECTED");
+})
+
+
+// my routes
+app.use("/api",authRoutes);
+
+// this is middleware
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+
+const port = process.env.PORT||8000;
+
+app.listen(port,()=>{
+    console.log(`App is running at ${port}`)
+})
